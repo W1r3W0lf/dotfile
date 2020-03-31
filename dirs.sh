6acd3b055128ead5c69bd1f75ca9f9d0630e6672
Michael Jury <6158203+W1r3W0lf@users.noreply.github.com>
2020-03-31 16:57:52 -0600

    Update dirs.sh

--- dirs.sh
+++ dirs.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 
-mkdir -p ~/bin		# A place for binary files setup by the user
-mkdir -p ~/local	# For local tools
-mkdir -p ~/projects	# For active projects
-mkdir -p ~/source	# Source code
-mkdir -p ~/apps		# A place for Appimages
+mkdir -p ~/bin      # A place for binary files setup by the user
+mkdir -p ~/local    # For local tools
+mkdir -p ~/pro    	# For active projects
+mkdir -p ~/src    	# Source code
+mkdir -p ~/app      # A place for Appimages
